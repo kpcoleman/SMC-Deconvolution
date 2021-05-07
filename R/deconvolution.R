c454b063946d97586924183da3358b1683c58fb5
kpcoleman <kylepcoleman@kyles-mbp-2.lan>
2021-05-07 11:55:21 -0400

    smc

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -1,4 +1,4 @@
-#' deconvolution
+#' prop_plot
 #'
 #' @param X cell-type specific GE data
 #' @param Y bulk GE data
@@ -144,12 +144,7 @@ prop_plot = function(X,Y,M,N=40,L=1, num_cores=1){
 }
 
 
-#scatter_plot = function(df){
-#  plot(df$true, df$est,col = df$color, pch = 16, cex = 2, ylim = c(0,1), xlab = 'True Mixture Proportion', ylab = 'Estimated mixture proportion')
-#  lines(seq(0,1,0.01),seq(0,1,0.01), lty = 3)
-#  legend(0.8,0.2, legend = c('ideal',rownames(M)[1],rownames(M)[2]), col = c('black','magenta','green'), lty = c(3,NA,NA), pch = c(NA,16,16))
-#  legend(0.1,0.9,legend = parse(text=sprintf('paste(r,\' = %s\')',round(cor(df$true, df$est),2))))
-#}
+
 
 
 
---
+++ R/scatter_plot.R
@@ -0,0 +1,16 @@
+#' scatter_plot
+#'
+#' @param df deconvolution results (columns = 'true','est','type','color')
+#'
+#'
+#'
+#'
+
+
+
+scatter_plot = function(df){
+  plot(df$true, df$est,col = df$color, pch = 16, cex = 2, ylim = c(0,1), xlab = 'True Mixture Proportion', ylab = 'Estimated mixture proportion')
+  lines(seq(0,1,0.01),seq(0,1,0.01), lty = 3)
+  legend(0.8,0.2, legend = c('ideal',rownames(M)[1],rownames(M)[2]), col = c('black','magenta','green'), lty = c(3,NA,NA), pch = c(NA,16,16))
+  legend(0.1,0.9,legend = parse(text=sprintf('paste(r,\' = %s\')',round(cor(df$true, df$est),2))))
+}
